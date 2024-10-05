7684218f71a0b4f0e507e0b780477771390cfa3f
Ivylucyawuor <awuorivy09>
2024-10-05 06:22:27 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,52 +1,25 @@
-#!/usr/bin/env python3
+# server/app.py
 
-from flask import Flask, make_response, jsonify
+from flask import Flask
 from flask_migrate import Migrate
 
-from models import db, Earthquake
+from models import db
 
+# create a Flask application object
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
+# configure a database connection to the local file app.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# disable modification tracking to use less memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# create a Migrate object to manage schema modifications
 migrate = Migrate(app, db)
+
+# initialize the Flask application to use the database
 db.init_app(app)
 
 
-@app.route('/earthquakes/<int:id>', methods=['GET'])
-def get_earthquake(id):
-    earthquake = db.session.get(Earthquake, id)  # Update this line
-    if earthquake:
-        return jsonify(earthquake.to_dict()), 200
-    else:
-        return jsonify({"message": f"Earthquake {id} not found."}), 404
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
-# Add views here
-@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
-def get_earthquakes_by_magnitude(magnitude):
-    # Query earthquakes with magnitude >= provided value
-    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
-
-    # Format the response data
-    quakes_data = [
-        {
-            "id": earthquake.id,
-            "location": earthquake.location,
-            "magnitude": earthquake.magnitude,
-            "year": earthquake.year
-        }
-        for earthquake in earthquakes
-    ]
-
-    # Construct the JSON response with the count and the list of earthquakes
-    response = {
-        "count": len(quakes_data),
-        "quakes": quakes_data
-    }
-
-    # Return the response with a 200 OK status
-    return jsonify(response), 200
+    app.run(port=5555, debug=True)
